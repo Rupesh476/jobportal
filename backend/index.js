ecae23091189adc91b1437803a4fbf15a014173e
Rupesh476 <sahrupesh288@gmail.com>
2025-05-15 13:21:20 +0545

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://jobportal-mksi.onrender.com/',
     credentials:true
 }
 
